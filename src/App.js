9b4e035369211390e446cd4added12248c4caa2a
Hayk An <say@hihayk.com>
2020-04-14 13:11:15 +0200

    gallery submissions

--- src/App.js
+++ src/App.js
@@ -491,6 +491,78 @@ class ScaleApp extends Component {
 
 const galleryData = [
   {
+    scaleValue: '#8/0/0/0/-53/-193/0/-100/0098FF/0/152/255',
+    authorName: 'Hayk',
+    authorLink: 'https://hihayk.com',
+  },
+  {
+    scaleValue: '#4/6/62/91/0/17/-36/14/3E83FF/62/131/255',
+    authorName: 'StrowBeary',
+    authorLink: 'https://remicaillot.fr/',
+  },
+  {
+    scaleValue: '#4/6/10/87/-103/146/39/-100/FFB177/255/177/119',
+    authorName: 'Kyle Hall',
+    authorLink: 'https://github.com/kyle-hall',
+  },
+  {
+    scaleValue: '#4/4/70/89/0/0/0/4/0859fC/8/89/252',
+    authorName: 'LizS',
+  },
+  {
+    scaleValue: '#4/6/51/83/-8/-8/74/77/269AC9/38/154/201',
+    authorName: 'Hum',
+    authorLink: 'https://vk.com/hummanoid',
+  },
+  {
+    scaleValue: '#2/6/25/80/8/-33/-7/47/F16334/241/99/52',
+    authorName: 'David Kariuki',
+    authorLink: 'https://www.behance.net/davellykariuki',
+  },
+  {
+    scaleValue: '#4/6/65/80/82/11/41/-1/A39A6C/163/154/108',
+    authorName: 'TheXCodeSeeker',
+  },
+  {
+    scaleValue: '#4/6/50/80/-12/77/33/-10/23A9CF/35/169/207',
+    authorName: 'Don',
+    authorLink: 'http://thinkrepeat.com/',
+  },
+  {
+    scaleValue: '#3/0/50/80/-51/67/20/14/C0DE58/192/222/88',
+    authorName: 'Pinki',
+    authorLink: 'https://dribbble.com/Pinki',
+  },
+  {
+    scaleValue: '#2/2/50/25/-50/-150/25/-50/E05040/224/80/64',
+    authorName: 'Dan Pontes',
+    authorLink: 'https://danpontes.webflow.io',
+  },
+  {
+    scaleValue: '#4/6/80/97/-51/-3/0/14/7d61dc/125/97/220',
+    authorName: 'Accessible Visual',
+    authorLink: 'https://www.freshworks.com/agile-project-management-software/?source=fworks&medium=referral&campaign=fworks_product_nav',
+  },
+  {
+    scaleValue: '#5/5/62/40/118/-172/-46/14/FFE64A/255/230/74',
+    authorName: 'Crshlab',
+    authorLink: 'http://crashlab.be',
+  },
+  {		
+    scaleValue: '#6/8/32/80/-31/67/5/100/1D9AFF/29/154/255',
+    authorName: 'Michael Andreuzza ',
+    authorLink: 'https://dribbble.com/MichaelAndreuzza',
+  },
+  {		
+    scaleValue: '#4/6/50/61/-51/17/63/14/1DBDC3/29/189/195',
+    authorName: 'Ali Rahmani',
+    authorLink: 'https://www.instagram.com/rahmaniali.ir/',
+  },     
+  {		
+    scaleValue: '#2/4/50/80/-51/67/20/14/a5b0feff/165/176/254',
+    authorName: 'Siar',
+  },   
+  {
     scaleValue: '#2/4/31/82/-8/-48/34/31/6a67b1/106/103/177',
     authorName: 'Hayk',
     authorLink: 'https://hihayk.com',
